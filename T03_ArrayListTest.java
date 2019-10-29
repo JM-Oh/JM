acf2e5bd37d3233dab04a47e6dcf8303d51d69c3
JM-OH <dun4602@gmail.com>
2019-10-29 19:18:43 +0900

    question

---
+++ T03_ArrayListTest.java
@@ -0,0 +1,14 @@
+package kr.or.ddit.basic;
+
+import java.util.Scanner;
+
+/*
+	문제) 5명의 사람이름을 입력하여 ArrayList에 저장하고 이 중에 '김'씨 성의 이름을 출력하시오.
+		입력은 Scanner를 이용하여 입력받는다.
+ */
+public class T03_ArrayListTest {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+	}
+}
